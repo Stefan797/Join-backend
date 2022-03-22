1322125eebb55ae2ed4f7b80cd9e7b0d9e223750
Stefan797 <stefanhubner97@web.de>
2022-03-22 18:30:39 +0100

    return board html

--- Joinbackend/Joinbackend/urls.py
+++ Joinbackend/Joinbackend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from board.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('board/', index),
 ]
--- Joinbackend/board/views.py
+++ Joinbackend/board/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+# from django.http import JsonResponse
+
+# from django.core import serializers
+
 # Create your views here.
+
+def index(request):
+    # if request.method == 'POST':
+    #     serialized_obj = serializers.serialize('json', [ obj, ])
+    return render(request, 'board/index.html')
+    
