646b25e3b005c7df8c24e1edcce0af34155d458b
Stefan797 <stefanhubner97@web.de>
2022-04-14 10:22:53 +0200

    crsf

--- Joinbackend/Joinbackend/settings.py
+++ Joinbackend/Joinbackend/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://kanbanboard12.herokuapp.com'
+]
+
 
 # Application definition
 
--- Joinbackend/board/views.py
+++ Joinbackend/board/views.py
@@ -39,10 +39,12 @@ import json
 #     return render(request, 'auth/register.html', )
 
 def index(request):
-    if request.method == 'GET':
-        return render(request, 'board/index.html')
     if request.method == 'POST':
-        new_task = Tasks.objects.create(text=request.POST['textvalue'], author=request.user, created_at=request.POST['datevalue'], color=request.POST['colorvalue'], discription=request.POST['discriptionvalue'], category=request.POST['value'])
+        new_task = Tasks.objects.create(text=request.POST['textvalue'], user=request.user, created_at=request.POST['datevalue'], color=request.POST['colorvalue'], discription=request.POST['discriptionvalue'], category=request.POST['category'])
+    
+    return render(request, 'board/index.html')
+
+
 
 def jsononeelement(request, id):
     if request.method == 'GET':
